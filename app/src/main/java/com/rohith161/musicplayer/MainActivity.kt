f6cf25d42c120aff6e9bdcc539fc36855027d858
Rohith S <rajrohith161@gmail.com>
2026-09-10 17:49:07 +0530

    Fix MainActivity Media3 opt-in lint error

--- app/src/main/java/com/rohith161/musicplayer/MainActivity.kt
+++ app/src/main/java/com/rohith161/musicplayer/MainActivity.kt
@@ -3,8 +3,10 @@ package com.rohith161.musicplayer
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.util.UnstableApi
 
 /** Compatibility entry point retained for older integrations. */
+@UnstableApi
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
